450fd33808b30d40b92f3f84005722217aa3b3b4
Kaz Nishimura <kazssym@linuxfront.com>
2025-09-21 18:57:11 +0900

    Add script to find and set MSVC version in GitHub Actions environment

---
+++ Find-MsvcVersion.ps1
@@ -0,0 +1,9 @@
+# Find-VsToolsVersion.ps1
+
+$VSWHERE = "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
+
+$VSINSTALLDIR = (& $VSWHERE -version "[17,18)" -property "installationPath") + "\"
+
+$VERSION = (Get-ChildItem -Path "${VSINSTALLDIR}VC\Tools\MSVC\" | Sort-Object -Property Name -Descending | Select-Object -First 1).Name
+
+"MSVC_VERSION=$VERSION" >> $env:GITHUB_ENV
