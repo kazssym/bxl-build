04af8b5ecb2a1bdad712177a43a349d1f456623e
Kaz Nishimura <kazssym@linuxfront.com>
2025-09-26 09:25:35 +0900

    Find-MsvcVersion.ps1 の更新

--- Find-MsvcVersion.ps1
+++ Find-MsvcVersion.ps1
@@ -1,4 +1,4 @@
-# Find-MsvcVersion.ps1
+# Find-MsvcVersion.ps1 (for GitHub actions)
 # Copyright 2025 Linuxfront.com
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
