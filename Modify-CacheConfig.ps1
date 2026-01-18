f86ac770aa7c72d5cb0f654fa564d63494b77dae
Kaz Nishimura <kazssym@linuxfront.com>
2026-01-18 15:13:48 +0900

    Add Modify-CacheConfig.ps1 script to update cache configuration

--- Modify-CacheConfig.ps1
+++ Modify-CacheConfig.ps1
@@ -1,3 +1,4 @@
+# Modify-CacheConfig.ps1
 # Copyright 2026 Linuxfront.com
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -13,8 +14,12 @@ param(
     [string]$Path
 )
 
+if (-not (Test-Path -Path $Path)) {
+    exit 1
+}
+
 #Requires -Version 6.0
-$Config = Get-Content -Raw $Path | ConvertFrom-Json
+$Config = Get-Content -Path $Path -Raw | ConvertFrom-Json
 
 $Config.Content.FileSystem.FilesystemL1.SizeInMegabytes = 1024
 
